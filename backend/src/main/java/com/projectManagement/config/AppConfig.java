69ec6338cb8a86c766fb07ce05017d21960ffe51
Rudraksh Agarwal <agrawal.samrat11173@gmail.com>
2024-12-26 17:22:37 +0530

    feat: add JWT authentication, login, and sign-up API.
    - Integrated JWT for secure token-based authentication
    - Implemented login and sign-up APIs
    - Configured Spring Security for authentication and authorization

---
+++ backend/src/main/java/com/projectManagement/config/AppConfig.java
@@ -0,0 +1,64 @@
+package com.projectManagement.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement(Management -> Management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll())
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
+        return http.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration corsConfig = new CorsConfiguration();
+                corsConfig.setAllowedOrigins(Arrays.asList(
+                        "http://localhost:3000/",
+                        "http://localhost:5173/",
+                        "http://localhost:4200/",
+                        "http://localhost:8080/"
+                ));
+
+                corsConfig.setAllowedMethods(Collections.singletonList("*"));
+                corsConfig.setAllowCredentials(true);
+                corsConfig.setAllowedHeaders(Collections.singletonList("*"));
+                corsConfig.setExposedHeaders(List.of("Authorization"));
+                corsConfig.setMaxAge(3600L);
+                return corsConfig;
+            }
+        };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
